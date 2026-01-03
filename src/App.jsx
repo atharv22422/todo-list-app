772df674fd8e97468d6e87f522d3886850b80133
AtharvBhavsar <atharvbhavsars@gmail.com>
2026-01-03 09:47:17 +0530

    hooks and event added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,77 @@
 import TodoHeading from "./components/TodoHeading";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
-import "./App.css";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
-  let TodoItems = [
+  // let TodoItems = [
+  //   {
+  //     name: "buy milk",
+  //     date: "4/10/2025",
+  //   },
+  //   {
+  //     name: "buy fish",
+  //     date: "5/10/2025",
+  //   },
+  //   {
+  //     name: "buy drink",
+  //     date: "6/10/2025",
+  //   },
+  // ];
+
+  const [TodoItems, setTodoItems] = useState([
     {
       name: "buy milk",
-      date: "4/10/2025",
+      date: "2026-01-03",
     },
     {
       name: "buy fish",
-      date: "5/10/2025",
+      date: "2026-01-03",
     },
     {
       name: "buy drink",
-      date: "6/10/2025",
+      date: "2026-01-03",
     },
-  ];
+  ]);
+
+  const [TodoItemName, setTodoItemName] = useState("");
+
+  const [TodoItemDate, setTodoItemDate] = useState("");
+
+  const OnDateChange = (event) => {
+    setTodoItemDate(event.target.value);
+  };
+
+  const OnTextChange = (event) => {
+    setTodoItemName(event.target.value);
+  };
+
+  const OnAddButtonClicked = () => {
+    setTodoItems((prevItems) => [
+      ...prevItems,
+      {
+        name: TodoItemName,
+        date: TodoItemDate,
+      },
+    ]);
+    setTodoItemDate("");
+    setTodoItemName("");
+  };
+
   return (
     <>
-      <div className="screen">
-        <div className="borderContainer">
+      <div className={styles.screen}>
+        <div className={styles.borderContainer}>
           <TodoHeading />
-          <div className="container kg-container">
-            <AddTodo />
+          <div className={`${styles.kgContainer} container`}>
+            <AddTodo
+              handleOnDateChange={OnDateChange}
+              handleOnTextChange={OnTextChange}
+              handleOnAddButtonClicked={OnAddButtonClicked}
+              TodoItemDate={TodoItemDate}
+              TodoItemName={TodoItemName}
+            />
             <TodoList TodoItems={TodoItems} />
           </div>
         </div>
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,12 @@
 import styles from "./AddTodo.module.css";
 
-function AddTodo() {
+function AddTodo({
+  handleOnTextChange,
+  handleOnDateChange,
+  handleOnAddButtonClicked,
+  TodoItemName,
+  TodoItemDate,
+}) {
   return (
     <>
       <div className={`row ${styles.KgRow}`}>
@@ -9,15 +15,23 @@ function AddTodo() {
             type="text"
             className={styles.AddTodo}
             placeholder="enter the item"
+            value={TodoItemName}
+            onChange={handleOnTextChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" className={styles.AddTodo} />
+          <input
+            type="date"
+            className={styles.AddTodo}
+            value={TodoItemDate}
+            onChange={handleOnDateChange}
+          />
         </div>
         <div className="col-2">
           <button
             type="button"
             className={`btn btn-success ${styles.KgButton}`}
+            onClick={handleOnAddButtonClicked}
           >
             ADD
           </button>
